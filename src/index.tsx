b7158805e560328a835e14c073869033ac4f15ad
Luks245 <florekl03c@lo5.bielsko.pl>
2023-05-21 18:26:10 +0200

    'm'

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './data/redux_store'
 import { PersistGate } from "redux-persist/integration/react"
@@ -16,11 +16,11 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter basename='/todo-app'>
+    <HashRouter basename='/todo-app'>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   // </React.StrictMode>
 );
