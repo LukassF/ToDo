7aaba66bab41e20754dca4c1c89b2aa8d554fa46
Luks245 <florekl03c@lo5.bielsko.pl>
2023-05-21 18:28:56 +0200

    'm'

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <HashRouter basename='/todo-app'>
+    <HashRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
